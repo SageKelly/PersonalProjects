f38da821a9c9a4f770b644f0e28ae7071606ace8
Sage Kelly <musicaltech120@bellsouth.net>
2014-12-13 09:24:25 -0500

    Animation Helper
    Another project I was working on to animated sprite sheets.

---
+++ Animation Helper/Animation Helper/Animation Helper/Program.cs
@@ -0,0 +1,21 @@
+using System;
+/*_1_0_0*/
+namespace Animation_Helper_V_1_5_0
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
