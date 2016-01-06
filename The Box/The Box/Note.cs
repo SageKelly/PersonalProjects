02c5f1797802ed0a0c6e348b5308bdb6a6f480c5
Sage Kelly <musicaltech120@bellsouth.net>
2016-01-05 22:44:53 -0500

    The Box Addition
    An Application for Windows Phone
    ♠ Great use for chord practice alongside a piano.

---
+++ The Box/The Box/Note.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Box
+{
+    public static class Note
+    {
+        private static string[] MajorNotes;
+        private static string[] Accidentals;
+        private static string[][] Notes;
+        //A♯ / B♭
+
+        /// <summary>
+        /// Will make a random table of Note objects
+        /// </summary>
+        /// <param name="table">the array in which to place the Note objects</param>
+        public static void MakeTable(out string[,] table)
+        {
+            table = new string[3, 4];
+
+            Random rand = new Random();
+
+            bool[] MajorBools = new bool[7] { false, false, false, false, false, false, false };
+            bool[] AcciBools = new bool[10] { false, false, false, false, false, false, false, false, false, false };
+            bool[][] Bools = new bool[2][] { MajorBools, AcciBools };
+
+            for (int i = 0; i < 12; i++)
+            {
+                int r = i / 3;
+                int c = i % 4;
+
+                int arr_num = rand.Next(0, 10) % 2;
+
+                int note_num = rand.Next(0, Bools[arr_num].Length);
+
+                while (Bools[arr_num][note_num])
+                {
+                    note_num = (note_num + 1) % Bools[arr_num].Length;
+                }
+
+                table[r, c] = Notes[arr_num][note_num];
+
+                Bools[arr_num][note_num] = true;
+                if (arr_num == 1)//If it's the boolean array...
+                {
+                    //check one more array as used.
+                    if (note_num % 2 == 0)
+                        AcciBools[note_num + 1] = true;
+                    else
+                        AcciBools[note_num - 1] = true;
+                }
+            }
+        }
+
+        public static List<string> MakeTable()
+        {
+            List<string> table = new List<string>();
+            SetupArrays();
+            Random rand = new Random();
+
+            bool[] MajorBools = new bool[7] { false, false, false, false, false, false, false };
+            bool[] AcciBools = new bool[10] { false, false, false, false, false, false, false, false, false, false };
+            bool[][] Bools = new bool[2][] { MajorBools, AcciBools };
+
+            for (int i = 0; i < 12; i++)
+            {
+                int arr_num = rand.Next(0, 10) % 2;
+
+                int note_num = rand.Next(0, Bools[arr_num].Length);
+
+                int j = 0;
+                while (Bools[arr_num][note_num])
+                {
+                    note_num = (note_num + 1) % Bools[arr_num].Length;
+                    j++;
+                    if (j >= Bools[arr_num].Length)
+                    {
+                        arr_num = (arr_num + 1) % 2;
+                        j = 0;
+                        note_num = 0;
+                    }
+#if DEBUG
+                    Debug.WriteLine("a: {0}, n: {1}, j: {2}", arr_num, note_num, j);
+#endif
+                }
+
+                table.Add(Notes[arr_num][note_num]);
+
+                Bools[arr_num][note_num] = true;
+                if (arr_num == 1)//If it's the boolean array...
+                {
+                    //check one more array as used.
+                    if (note_num % 2 == 0)
+                        AcciBools[note_num + 1] = true;
+                    else
+                        AcciBools[note_num - 1] = true;
+                }
+            }
+            return table;
+        }
+
+        private static void SetupArrays()
+        {
+            MajorNotes = new string[7];
+            MajorNotes[0] = "A";
+            MajorNotes[1] = "B";
+            MajorNotes[2] = "C";
+            MajorNotes[3] = "D";
+            MajorNotes[4] = "E";
+            MajorNotes[5] = "F";
+            MajorNotes[6] = "G";
+
+            Accidentals = new string[10];
+            Accidentals[0] = "A♯";
+            Accidentals[1] = "B♭";
+            Accidentals[2] = "C♯";
+            Accidentals[3] = "D♭";
+            Accidentals[4] = "D♯";
+            Accidentals[5] = "E♭";
+            Accidentals[6] = "F♯";
+            Accidentals[7] = "G♭";
+            Accidentals[8] = "G♯";
+            Accidentals[9] = "A♭";
+
+            Notes = new string[2][];
+            Notes[0] = MajorNotes;
+            Notes[1] = Accidentals;
+        }
+    }
+}
---
+++ The Box/The Box/PivotPage.xaml.cs
@@ -0,0 +1,151 @@
+﻿using The_Box.Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Resources;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.ComponentModel;
+
+// The Pivot Application template is documented at http://go.microsoft.com/fwlink/?LinkID=391641
+
+namespace The_Box
+{
+    public sealed partial class PivotPage : Page, INotifyPropertyChanged
+    {
+        private List<string> box;
+        public List<string> Box
+        {
+            get
+            {
+                return box;
+            }
+            set
+            {
+                if (box != value)
+                {
+                    box = value;
+                    OnPropertyChanged("Box");
+                }
+            }
+        }
+
+        private readonly NavigationHelper navigationHelper;
+        private readonly ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView("Resources");
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PivotPage()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>.
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache. Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/>.</param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+            // TODO: Save the unique state of the page here.
+        }
+
+        /// <summary>
+        /// Adds an item to the list when the app bar button is clicked.
+        /// </summary>
+        private void AddAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Box = Note.MakeTable();
+        }
+
+        private void OnPropertyChanged(string property_name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property_name));
+            }
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void MakeTable(object sender, TappedRoutedEventArgs e)
+        {
+            Box = Note.MakeTable();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Box = Note.MakeTable();
+        }
+    }
+}
