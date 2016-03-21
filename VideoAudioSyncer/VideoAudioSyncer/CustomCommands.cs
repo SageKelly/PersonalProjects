e50be69b357dab00146d796fa7540de20b486ca0
Sage Kelly <musicaltech120@bellsouth.net>
2016-03-21 01:19:21 -0400

    Mini-Update
    • Started fleshing out some event-handler methods
    • UI pretty much settled now.
    • Have to figure out Slider issue (stores values as doubles and not
    TimeSpans)
    • Have to add the ability to save profiles
    • Have to figure out audio / video bookmarking

---
+++ VideoAudioSyncer/VideoAudioSyncer/CustomCommands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VideoAudioSyncer
+{
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand SaveProfile = new RoutedUICommand(
+            "Save Profile",
+            "Save Profile",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.S, ModifierKeys.Control) 
+            }
+            );
+
+        public static readonly RoutedUICommand Exit = new RoutedUICommand(
+            "Exit",
+            "Exit",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4,ModifierKeys.Alt)
+            }
+            );
+
+    }
+}
--- VideoAudioSyncer/VideoAudioSyncer/MainWindow.xaml.cs
+++ VideoAudioSyncer/VideoAudioSyncer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,30 +21,64 @@ namespace VideoAudioSyncer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
 
         private bool mediaPlayerIsPlaying = false;
         private bool userIsDraggingSlider = false;
+        private const double _VOLUME_DELTA = .025;
+
+        /// <summary>
+        /// Represents the ProgressBar the mouse is over
+        /// </summary>
+        ProgressBar pbMouseOver;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private TimeSpan _syncProgress;
+        public TimeSpan SyncProgress
+        {
+            get { return _syncProgress; }
+            set
+            {
+                if (_syncProgress != value)
+                {
+                    _syncProgress = value;
+                    NotifyPropertyChanged("SyncProgress");
+                }
+            }
+        }
+
+        DispatcherTimer _vidTimer;
+        DispatcherTimer _audTimer;
+
+
 
         public MainWindow()
         {
             InitializeComponent();
-
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += timer_Tick;
-            timer.Start();
+            DataContext = this;
+            _vidTimer = new DispatcherTimer();
+            _audTimer = new DispatcherTimer();
         }
+        #region Methods
 
-
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #region Event-based
         private void timer_Tick(object sender, EventArgs e)
         {
             if ((audioPlayer.Source != null) && (audioPlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
             {
-                sliProgress.Minimum = 0;
-                sliProgress.Maximum = audioPlayer.NaturalDuration.TimeSpan.TotalSeconds;
-                sliProgress.Value = audioPlayer.Position.TotalSeconds;
+                sliSyncProgress.Minimum = 0;
+                sliSyncProgress.Maximum = audioPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                sliSyncProgress.Value = audioPlayer.Position.TotalSeconds;
+
             }
         }
 
@@ -110,18 +145,74 @@ namespace VideoAudioSyncer
         private void sliProgress_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
         {
             userIsDraggingSlider = false;
-            videoPlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
-            audioPlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+            videoPlayer.Position = TimeSpan.FromSeconds(sliSyncProgress.Value);
+            audioPlayer.Position = TimeSpan.FromSeconds(sliSyncProgress.Value);
         }
 
         private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            lblProgressStatus.Text = sliProgress.Value.ToString("hh:mm:ss");
+            SyncProgress = sliSyncProgress.Value.ToString("hh:mm:ss");
         }
 
         private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            audioPlayer.Volume = e.Delta > 0 ? 0.1 : -0.1;
+            MediaElement sentinel = audioPlayer;
+
+            double delta = e.Delta > 0 ? 0.1 : -0.1;
+
+            Point mousePos = e.MouseDevice.GetPosition(pbAudVolume);
+            if (pbMouseOver != pbSyncVolume)
+            {
+                if (pbMouseOver == pbAudVolume)
+                {
+                    sentinel = audioPlayer;
+                }
+                else if (pbMouseOver == pbVidVolume)
+                {
+                    sentinel = videoPlayer;
+                }
+                else
+                {
+                    delta = 0;
+                }
+                if (sentinel.Volume + delta >= 0 && sentinel.Volume + delta <= 1)
+                {
+                    sentinel.Volume += delta;
+                }
+                pbSyncVolume.Value = Math.Min(audioPlayer.Volume, videoPlayer.Volume);
+            }
+            else
+            {
+                pbSyncVolume.Value += delta;
+                audioPlayer.Volume = pbSyncVolume.Value;
+                videoPlayer.Volume = pbSyncVolume.Value;
+            }
         }
+
+        private void SaveProfile_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveProfile_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+
+        private void pb_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ProgressBar sentinel = sender as ProgressBar;
+            if (sentinel != null)
+            {
+                pbMouseOver = sentinel;
+            }
+        }
+
+        private void pb_MouseLeave(object sender, MouseEventArgs e)
+        {
+            pbMouseOver = null;
+        }
+        #endregion
+        #endregion
     }
 }
